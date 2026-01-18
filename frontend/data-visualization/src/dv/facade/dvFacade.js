0f32213c957e62280082f62c98926a95009dac5a
Julia <250353@edu.p.lodz.pl>
2026-01-18 13:13:10 +0100

    login, dashboard

--- frontend/data-visualization/src/dv/facade/dvFacade.js
+++ frontend/data-visualization/src/dv/facade/dvFacade.js
@@ -4,18 +4,51 @@ import { rawMetadata } from '../mock_data/rawMetadata';
 
 const API_BASE_URL = "http://127.0.0.1:8000/api/v1/gateway";
 
-function getAuthToken() {
-  return localStorage.getItem('access_token') || "token_for_user"; 
+function getAuthHeaders() {
+  const token = localStorage.getItem('access_token');
+  return {
+    'Authorization': `Bearer ${token}`,
+    'Content-type': 'application/json'
+  }
+}
+
+// --- LOGIN ---
+export async function loginUser(username, password) {
+  try {
+    const response = await fetch(`${API_BASE_URL}/login`, {
+      method: 'POST',
+      headers: {
+        'Content-type': 'application/json',
+      },
+      body: JSON.stringify({
+        username: username,
+        password: password
+      })
+    });
+    if(!response.ok) {
+      return { status: 'error', message: 'Invalid credentials'};
+    }
+    const data = await response.json();
+
+    localStorage.setItem('access_token', data.access_token);
+    localStorage.setItem('userRole', data.role);
+
+    return { status: 'ok', role: data.role};
+  }
+  catch (error) {
+    console.error("Login error:", error);
+    return {status: 'error', message: 'Server connection failed'};
+  }
 }
 
 // --- ALERTS ---
 async function fetchAlertsFromAAC(query) {
   try {
-    const token = getAuthToken();
     const url = new URL(`${API_BASE_URL}/alerts`);
-    url.searchParams.append("token", token);
-    
-    const response = await fetch(url);
+    url.searchParams.append("buildingId", query.buildingId || "Building-1");
+    url.searchParams.append("fromDate", query.timeRange?.from || new Date(Date.now() - 86400000).toISOString());
+    url.searchParams.append("toDate", query.timeRange?.to || new Date().toISOString());
+    const response = await fetch(url, {headers: getAuthHeaders()});
     if (!response.ok) return [];
     return await response.json();
   } catch (error) {
@@ -38,17 +71,15 @@ export async function getAlerts(inputParams) {
 // --- MEASUREMENTS ---
 async function fetchMeasurements(query) {
   try {
-    const token = getAuthToken();
     const url = new URL(`${API_BASE_URL}/measurements`);
     
     // Dodajemy parametry wymagane przez backend
-    url.searchParams.append("token", token);
     url.searchParams.append("buildingId", query.buildingId || "Building-1");
     
-    const from = query.fromDate ?? "2026-01-01T00:00:00";
-    const to   = query.toDate   ?? "2026-01-30T23:59:59";
-    url.searchParams.append("start", from);
-    url.searchParams.append("end", to);
+    const from = query.timeRange?.from ?? new Date(Date.now() - 86400000).toISOString();
+    const to   = query.timeRange.to ?? new Date().toISOString();
+    url.searchParams.append("fromDate", from);
+    url.searchParams.append("toDate", to);
 
     // Mapowanie nazw metryk (Frontend -> Backend)
     const metricMap = {
@@ -66,7 +97,7 @@ async function fetchMeasurements(query) {
         url.searchParams.append("deviceId", query.deviceId);
     }
 
-    const response = await fetch(url);
+    const response = await fetch(url, {headers: getAuthHeaders()});
     if (!response.ok) {
         console.error("Błąd API Measurements:", response.status);
         return [];
@@ -99,21 +130,18 @@ export async function getMeasurements(inputParams) {
 // --- FORECASTS ---
 async function fetchForecasts(query) {
   try {
-    const token = getAuthToken();
     const url = new URL(`${API_BASE_URL}/forecasts`);
-    url.searchParams.append("token", token);
     url.searchParams.append("buildingId", query.buildingId || "Building-1");
     
-	  if (query.fromDate) {
-        url.searchParams.append("start", query.fromDate); 
+	  if (query.timeRange?.from) {
+        url.searchParams.append("fromDate", query.timeRange.from); 
     }
-    if (query.toDate) {
-        url.searchParams.append("end", query.toDate);
+    if (query.timeRange?.to) {
+        url.searchParams.append("toDate", query.timeRange.to);
     }
-    url.searchParams.append("type", query.type || "temperature");
+    url.searchParams.append("type", query.type || "energy_demand");
 
-    
-    const response = await fetch(url);
+    const response = await fetch(url, {headers: getAuthHeaders()});
     if (!response.ok) return null;
     
     return await response.json();
@@ -138,4 +166,21 @@ export async function getForecasts(inputParams) {
   const meta = rawMetadata;
   const formatted = formatForecasts(rawData, meta);
   return {status: 'ok', data: formatted};
+}
+
+// --- DASHBOARD ---
+export async function getDashboardStats(buildingId = "Building-1") {
+  try {
+    const url = new URL(`${API_BASE_URL}/dashboard`);
+    url.searchParams.append("buildingId", buildingId);
+
+    const response = await fetch(url, {headers: getAuthHeaders()});
+
+    if(!response.ok) return null;
+    return await response.json();
+  }
+  catch (e) {
+    console.error("Dashboard stats error", e);
+    return null
+  }
 }
